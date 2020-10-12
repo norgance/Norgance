6879de6ae3a8090911a602986345637e7d11049a
Norgance <66333061+norgance-admin@users.noreply.github.com>
2020-10-12 09:52:50 +0200

    🙈 Fix base64 constraints

--- backend/norgance/migrations/2020-09-18-082911_create_schema/up.sql
+++ backend/norgance/migrations/2020-09-18-082911_create_schema/up.sql
@@ -7,28 +7,28 @@ CREATE TABLE citizens (
     PRIMARY KEY
     NOT NULL
     CONSTRAINT valid_identifier
-      CHECK (identifier ~ '^[a-zA-Z0-9]{64}$'),
+      CHECK (identifier ~ '^[a-zA-Z0-9+/]{64}$'),
   access_key TEXT
     NOT NULL
     CONSTRAINT valid_access_key
-      CHECK (access_key ~ '^[a-zA-Z0-9]{64}$'
+      CHECK (access_key ~ '^[a-zA-Z0-9+/]{64}$'
         AND access_key <> identifier),
   public_x448 TEXT
     NOT NULL
     CONSTRAINT valid_public_x448
-      CHECK (public_x448 ~ '^[a-zA-Z0-9]+$'),
+      CHECK (public_x448 ~ '^[a-zA-Z0-9+/]+$'),
   public_x25519_dalek TEXT
     NOT NULL
     CONSTRAINT valid_public_x25519_dalek
-      CHECK (public_x25519_dalek ~ '^[a-zA-Z0-9]+$'),
+      CHECK (public_x25519_dalek ~ '^[a-zA-Z0-9+/]+$'),
   public_ed25519_dalek TEXT
     NOT NULL
     CONSTRAINT valid_public_ed25519_dalek
-      CHECK (public_ed25519_dalek ~ '^[a-zA-Z0-9]+$'),
+      CHECK (public_ed25519_dalek ~ '^[a-zA-Z0-9+/]+$'),
   aead_data TEXT
     NOT NULL
-    CONSTRAINT valid_aead_daata
-      CHECK (aead_data ~ '^[a-zA-Z0-9]+$')
+    CONSTRAINT valid_aead_data
+      CHECK (aead_data ~ '^[a-zA-Z0-9+/]+$')
 );
 
 CREATE TABLE identity_documents (
@@ -36,7 +36,7 @@ CREATE TABLE identity_documents (
     PRIMARY KEY
     NOT NULL 
     CONSTRAINT valid_identity_document_hash
-      CHECK (identity_document_hash ~ '^[a-zA-Z0-9]{64}$'),
+      CHECK (identity_document_hash ~ '^[a-zA-Z0-9+/]{64}$'),
   citizen_identifier TEXT
     NOT NULL
     REFERENCES citizens(identifier)
@@ -44,7 +44,7 @@ CREATE TABLE identity_documents (
   ed25519_dalek_signature TEXT
     NOT NULL
     CONSTRAINT valid_ed25519_dalek_signature
-      CHECK (ed25519_dalek_signature ~ '^[a-zA-Z0-9]+$')
+      CHECK (ed25519_dalek_signature ~ '^[a-zA-Z0-9+/]+$')
 );
 
 CREATE TABLE shared_documents (
@@ -52,14 +52,14 @@ CREATE TABLE shared_documents (
     PRIMARY KEY
     NOT NULL
     CONSTRAINT valid_identifier
-      CHECK (identifier ~ '^[a-zA-Z0-9]{64}$'),
+      CHECK (identifier ~ '^[a-zA-Z0-9+/]{64}$'),
   
   aead_data TEXT
     NOT NULL
-    CONSTRAINT valid_aead_daata
-      CHECK (aead_data ~ '^[a-zA-Z0-9]+$'),
+    CONSTRAINT valid_aead_data
+      CHECK (aead_data ~ '^[a-zA-Z0-9+/]+$'),
   data_ed25519_dalek_signature TEXT
     NOT NULL
     CONSTRAINT valid_data_ed25519_dalek_signature
-      CHECK (data_ed25519_dalek_signature ~ '^[a-zA-Z0-9]+$')
+      CHECK (data_ed25519_dalek_signature ~ '^[a-zA-Z0-9+/]+$')
 );
