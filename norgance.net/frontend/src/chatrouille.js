9f62fdaa7317f1585740982a4c37510e43b0c2aa
Norgance <66333061+norgance-admin@users.noreply.github.com>
2020-10-14 22:07:03 +0200

    🐱 Chatrouille client in the frontend

---
+++ norgance.net/frontend/src/chatrouille.js
@@ -0,0 +1,91 @@
+import ky from 'ky';
+import {
+  chatrouillePackUnsignedQuery,
+  chatrouilleUnpackResponse,
+} from './rust';
+
+const publicKey = Uint8Array.from([
+  62,
+  183,
+  168,
+  41,
+  176,
+  205,
+  32,
+  245,
+  188,
+  252,
+  11,
+  89,
+  155,
+  111,
+  236,
+  207,
+  109,
+  164,
+  98,
+  113,
+  7,
+  189,
+  176,
+  212,
+  243,
+  69,
+  180,
+  48,
+  39,
+  216,
+  185,
+  114,
+  252,
+  62,
+  52,
+  251,
+  66,
+  50,
+  161,
+  60,
+  167,
+  6,
+  220,
+  181,
+  122,
+  236,
+  61,
+  174,
+  7,
+  189,
+  193,
+  198,
+  123,
+  243,
+  54,
+  9,
+]);
+
+class GraphqlError extends Error {
+  constructor(errors) {
+    super('Error from the GraphQL server');
+    this.name = this.constructor.name;
+    this.errors = errors;
+  }
+}
+
+export async function anonymousQuery(graphql) {
+  const payload = JSON.stringify({ graphql });
+  const { query, sharedSecret } = await chatrouillePackUnsignedQuery(payload, publicKey);
+  const response = await ky.post('http://localhost:3000/chatrouille', {
+    body: query,
+  });
+  const responseBody = await response.arrayBuffer();
+  const decoded = await chatrouilleUnpackResponse(new Uint8Array(responseBody), sharedSecret);
+  const jsonResponse = JSON.parse(decoded);
+  if (jsonResponse.errors && Array.isArray(jsonResponse.errors) && jsonResponse.errors.length > 0) {
+    throw new GraphqlError(jsonResponse.errors);
+  }
+  return jsonResponse.data;
+}
+
+export async function authenticatedQuery(/* graphql, citizenIdentifier, citizenPrivateKey */) {
+  throw new Error('TODO');
+}
