85b350500cae67fe8584a5da72ba71a6f4a91a6c
Norgance <66333061+norgance-admin@users.noreply.github.com>
2020-10-16 19:46:08 +0200

    🚛 remove dependency to promise-worker
    Reimplement it, but only for our usecase. Supporting transferables ArrayBuffers :-)

--- norgance.net/frontend/src/rust.js
+++ norgance.net/frontend/src/rust.js
@@ -1,51 +1,97 @@
-import PromiseWorker from 'promise-worker';
 import mem from 'mem';
+
+class PromiseWorker {
+  constructor(worker) {
+    this.worker = worker;
+    this.callbacks = new Map();
+    this.currentMessageId = 0;
+
+    worker.addEventListener('message', this.onMessage.bind(this));
+  }
+
+  onMessage(event) {
+    const data = event.data;
+    if (!data) return;
+
+    const { messageId } = data;
+    const callback = this.callbacks.get(messageId);
+
+    // Ignore messages that are not from us
+    if (!callback) {
+      return;
+    }
+
+    callback(data);
+    this.callbacks.delete(messageId);
+  }
+
+  call(functionName, args = []) {
+    const messageId = this.currentMessageId;
+    this.currentMessageId += 1;
+
+    if (this.currentMessageId > Number.MAX_SAFE_INTEGER) {
+      // We assume we got all the previous callbacks
+      // If not, something very wrong did happen
+      this.currentMessageId = 0;
+    }
+
+    const data = {
+      messageId,
+      functionName,
+      args,
+    };
+
+    return new Promise((resolve, reject) => {
+      this.callbacks.set(messageId, ({ error, response }) => {
+        if (error) {
+          reject(error);
+        } else {
+          resolve(response);
+        }
+      });
+
+      this.worker.postMessage(data, args.filter((arg) => arg instanceof ArrayBuffer));
+    });
+  }
+}
 
 const worker = new Worker('./rustWorker.js', { name: 'rustWorker', type: 'module' });
 const promiseWorker = new PromiseWorker(worker);
 
-export const norganceIdentifier = mem((identifier) => promiseWorker.postMessage({
-  function: 'norgance_identifier',
-  args: [identifier],
-}));
+export const norganceIdentifier = mem((identifier) => promiseWorker.call(
+  'norgance_identifier',
+  [identifier],
+));
 
 export const norganceCitizenSymmetricKey = mem(
-  (identifier, password) => promiseWorker.postMessage({
-    function: 'norgance_citizen_symmetric_key',
-    args: [identifier, password],
-  }), {
+  (identifier, password) => promiseWorker.call('norgance_citizen_symmetric_key',
+    [identifier, password]), {
     cacheKey: JSON.stringify,
   },
 );
 
 export const norganceCitizenAccessKey = mem(
-  (identifier, password) => promiseWorker.postMessage({
-    function: 'norgance_citizen_access_key',
-    args: [identifier, password],
-  }), {
+  (identifier, password) => promiseWorker.call('norgance_citizen_access_key',
+    [identifier, password]), {
     cacheKey: JSON.stringify,
   },
 );
 
-export const norganceHibpPasswordHash = mem((password, size = 20) => promiseWorker.postMessage({
-  function: 'norgance_hibp_password_hash',
-  args: [password, size],
-}), {
+export const norganceHibpPasswordHash = mem((password, size = 20) => promiseWorker.call('norgance_hibp_password_hash',
+  [password, size]), {
   cacheKey: JSON.stringify,
 });
 
+require('./rustWorker');
+
 export function chatrouillePackUnsignedQuery(payload, publicKey) {
-  return promiseWorker.postMessage({
-    function: 'chatrouille_pack_unsigned_query',
-    args: [payload, publicKey],
-  });
+  return promiseWorker.call('chatrouille_pack_unsigned_query',
+    [payload, publicKey]);
 }
 
 export function chatrouilleUnpackResponse(packedData, sharedSecret) {
-  return promiseWorker.postMessage({
-    function: 'chatrouille_unpack_response',
-    args: [packedData, sharedSecret],
-  });
+  return promiseWorker.call('chatrouille_unpack_response',
+    [packedData, sharedSecret]);
 }
 
 export default promiseWorker;
--- norgance.net/frontend/src/rustWorker.js
+++ norgance.net/frontend/src/rustWorker.js
@@ -1,29 +1,57 @@
-import registerPromiseWorker from 'promise-worker/register';
-
 let rust;
+self.addEventListener('message', async (event) => {
+  const { messageId, functionName, args } = event.data;
 
-registerPromiseWorker(async (message) => {
-  if (!rust) {
-    rust = await import('../rust/pkg');
-  }
-  const functionName = message.function;
-  if (typeof rust[functionName] !== 'function') {
-    throw new TypeError(`Unknown function ${functionName}`);
+  if (typeof messageId === 'undefined') {
+    return;
   }
 
-  const returnObject = await rust[functionName](...message.args);
+  try {
+    // Lazy loading
+    if (!rust) {
+      rust = await import('../rust/pkg');
+    }
 
-  if (returnObject instanceof rust.ChatrouilleUnsignedQuery) {
-    // We need to build the final object in the worker
-    const finalObject = {
-      query: returnObject.get_query(),
-      sharedSecret: returnObject.get_shared_secret(),
-    };
+    if (typeof rust[functionName] !== 'function') {
+      throw new TypeError(`Unknown function ${functionName}`);
+    }
 
-    // It's important to not leak memory
-    returnObject.free();
-    return finalObject;
+    let returnObject = await rust[functionName](...args);
+    let transferables;
+
+    if (returnObject instanceof rust.ChatrouilleUnsignedQuery) {
+      // We need to build the final object in the worker
+      const finalObject = {
+        query: returnObject.get_query(),
+        sharedSecret: returnObject.get_shared_secret(),
+      };
+
+      transferables = [
+        finalObject.query.buffer,
+        finalObject.sharedSecret.buffer,
+      ];
+
+      // It's important to not leak memory
+      returnObject.free();
+      returnObject = finalObject;
+    } else if (returnObject) {
+      if (returnObject instanceof ArrayBuffer) {
+        transferables = [returnObject];
+      } else if (returnObject.buffer instanceof ArrayBuffer) {
+        transferables = [returnObject.buffer];
+      }
+    }
+
+    self.postMessage({
+      messageId,
+      response: returnObject,
+    }, transferables);
+
+  } catch (error) {
+    console.error(error);
+    self.postMessage({
+      messageId,
+      error,
+    });
   }
-
-  return returnObject;
 });
